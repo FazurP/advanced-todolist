d6328976f4d1386f6037cf6669bc45854b277246
Fazur Alejandro Estrada Ponce <limon-amargoverde15@hotmail.com>
2021-08-28 01:57:34 -0500

    first commit

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname+'/dist/advanced-todo-list'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/advanced-todo-list/index.html'));
+    res.sendFile('index.html',{root:'dist/advanced-todo-list/'});
 });
+
+app.listen(process.env.PORT || 3000)
